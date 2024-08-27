cbd7c6e3516e66de73f2841bf7b5857d4f1073fe
Aleksandra <117662213+hackbutnotme@users.noreply.github.com>
2024-08-27 22:26:06 +0200

    Add files via upload

---
+++ decrypt_logs.py
@@ -0,0 +1,17 @@
+from cryptography.fernet import Fernet
+
+with open('key.key', 'rb') as f:
+    key = f.read()
+    
+cipher_suite = Fernet(key)
+
+with open('klogs.txt', 'rb') as f:
+    encrypted_logs = f.readlines()
+    
+decrypted_logs = []
+decrypted_logs = cipher_suite.decrypt(encrypted_logs)
+#print(decrypted_logs.decode())
+with open('decrypted_logs.txt','w') as f:
+   f.write(decrypted_logs.decode())
+    
+    
