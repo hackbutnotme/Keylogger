2397713ae8c4843c20e34a4c4a723aa4de8cdadc
Aleksandra <117662213+hackbutnotme@users.noreply.github.com>
2024-08-29 15:04:13 +0200

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet
+import sys 
 
 with open('key.key', 'rb') as f:
     key = f.read()
@@ -8,10 +9,15 @@ cipher_suite = Fernet(key)
 with open('klogs.txt', 'rb') as f:
     encrypted_logs = f.readlines()
     
-decrypted_logs = []
-decrypted_logs = cipher_suite.decrypt(encrypted_logs)
+decrypted_logs = ''
+for log in encrypted_logs:
+    decrypted_log = cipher_suite.decrypt(log.strip()).decode()
+    decrypted_logs += decrypted_log + '\n'
 
 with open('decrypted_logs.txt','w') as f:
-   f.write(decrypted_logs.decode())
+   f.write(decrypted_logs)
+
+print("Decryption complete.")
+sys.exit(0)
     
     
